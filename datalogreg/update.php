a1113ad79f610ada7af81defd2ea5ad14b0a43fa
Ayuistawati <115130993+Ayuistawati@users.noreply.github.com>
2023-06-10 23:49:28 +0800

    Add files via upload

---
+++ datalogreg/update.php
@@ -0,0 +1,94 @@
+<!-- update.php -->
+<?php
+session_start();
+require_once 'config.php';
+
+if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
+    header('Location: index.php');
+    exit;
+}
+
+// Jika parameter id tidak ada, kembali ke halaman admin_dashboard.php
+if (!isset($_GET['id'])) {
+    header('Location: admin_dashboard.php');
+    exit;
+}
+
+$id = $_GET['id'];
+
+// Mengecek apakah data dengan id tersebut ada di MongoDB
+$filter = ['_id' => new MongoDB\BSON\ObjectID($id)];
+$query = new MongoDB\Driver\Query($filter);
+$result = $mongo->executeQuery('db_logreg.tb_register', $query);
+$document = $result->toArray()[0];
+
+// Jika data tidak ditemukan, kembali ke halaman admin_dashboard.php
+if (!$document) {
+    header('Location: admin_dashboard.php');
+    exit;
+}
+
+// Jika tombol update ditekan
+if (isset($_POST['update'])) {
+    // Mengambil data dari form
+    $username = $_POST['username'];
+    $gmail = $_POST['gmail'];
+    $alamat = $_POST['alamat'];
+    $no_tlp = $_POST['no_tlp'];
+
+    // Menyiapkan data untuk diupdate di MongoDB
+    $newDocument = [
+        '$set' => [
+            'username' => $username,
+            'gmail' => $gmail,
+            'alamat' => $alamat,
+            'no_tlp' => $no_tlp
+        ]
+    ];
+
+    // Membuat perintah untuk update data
+    $bulkWrite = new MongoDB\Driver\BulkWrite;
+    $bulkWrite->update($filter, $newDocument);
+
+    // Menjalankan perintah update
+    $mongo->executeBulkWrite('db_logreg.tb_register', $bulkWrite);
+
+    // kembali ke halaman admin_dashboard.php setelah update
+    header('Location: admin_dashboard.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Update Data</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Update Data</h2>
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="username">Username:</label>
+                <input type="text" name="username" id="username" value="<?php echo $document->username; ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="gmail">Gmail:</label>
+                <input type="email" name="gmail" id="gmail" value="<?php echo $document->gmail; ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="alamat">Alamat:</label>
+                <input type="text" name="alamat" id="alamat" value="<?php echo $document->alamat; ?>" required>
+            </div>
+            <div class="form-group">
+                <label for="no_tlp">No. Telepon:</label>
+                <input type="text" name="no_tlp" id="no_tlp" value="<?php echo $document->no_tlp; ?>" required>
+            </div>
+            <div class="form-group">
+                <button type="submit" name="update">Update</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ datalogreg/user_dashboard.php
@@ -0,0 +1,141 @@
+<?php
+session_start();
+require_once 'config.php';
+
+if (!isset($_SESSION['username'])) {
+    header('Location: index.php');
+    exit;
+}
+
+// Fungsi untuk mendapatkan data pengguna berdasarkan username
+function getUserData($username)
+{
+    global $mongo;
+
+    $query = new MongoDB\Driver\Query(['username' => $username]);
+    $result = $mongo->executeQuery('db_logreg.tb_register', $query)->toArray();
+
+    if (!empty($result)) {
+        return $result[0];
+    } else {
+        return null;
+    }
+}
+
+// Fungsi untuk mengupdate data pengguna
+function updateUserData($username, $data)
+{
+    global $mongo;
+
+    $bulkWrite = new MongoDB\Driver\BulkWrite;
+    $bulkWrite->update(['username' => $username], ['$set' => $data]);
+
+    $mongo->executeBulkWrite('db_logreg.tb_register', $bulkWrite);
+}
+
+// Fungsi untuk menghapus data pengguna
+function deleteUserData($username)
+{
+    global $mongo;
+
+    $bulkWrite = new MongoDB\Driver\BulkWrite;
+    $bulkWrite->delete(['username' => $username]);
+
+    $mongo->executeBulkWrite('db_logreg.tb_register', $bulkWrite);
+}
+
+// Memperbarui data pengguna
+if (isset($_POST['update'])) {
+    $username = $_SESSION['username'];
+    $newGmail = $_POST['gmail'];
+    $newAddress = $_POST['alamat'];
+    $newPhone = $_POST['no_tlp'];
+
+    $data = [
+        'gmail'  => $newGmail,
+        'alamat' => $newAddress,
+        'no_tlp' => $newPhone
+    ];
+
+    updateUserData($username, $data);
+}
+
+// Menghapus data pengguna
+if (isset($_POST['delete'])) {
+    $username = $_SESSION['username'];
+    deleteUserData($username);
+
+    session_unset();
+    session_destroy();
+    header('Location: index.php');
+    exit;
+}
+
+// Mendapatkan data pengguna yang sedang login
+$username = $_SESSION['username'];
+$userData = getUserData($username);
+
+if (!$userData) {
+    // Jika data pengguna tidak ditemukan, redirect ke halaman login
+    header('Location: index.php');
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Dashboard</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h2>Welcome, <?php echo $username; ?> (User)</h2>
+        <h3>Profile</h3>
+        <table>
+            <tr>
+                <td>Username:</td>
+                <td><?php echo $userData->username; ?></td>
+            </tr>
+            <tr>
+                <td>Gmail:</td>
+                <td><?php echo $userData->gmail; ?></td>
+            </tr>
+            <tr>
+                <td>Alamat:</td>
+                <td><?php echo $userData->alamat; ?></td>
+            </tr>
+            <tr>
+                <td>No. Telepon:</td>
+                <td><?php echo $userData->no_tlp; ?></td>
+            </tr>
+        </table>
+
+        <h3>Edit Profile</h3>
+        <form action="" method="post">
+            <div class="form-group">
+                <label for="alamat">gmail:</label>
+                <input type="text" name="gmail" id="gmail" value="<?php echo $userData->gmail; ?>">
+            </div>
+            <div class="form-group">
+                <label for="no_tlp">alamat:</label>
+                <input type="text" name="alamat" id="alamat" value="<?php echo $userData->alamat; ?>">
+            </div>
+            <div class="form-group">
+                <label for="no_tlp">No. Telepon:</label>
+                <input type="text" name="no_tlp" id="no_tlp" value="<?php echo $userData->no_tlp; ?>">
+            </div>
+            <div class="form-group">
+                <button type="submit" name="update">Update Profile</button>
+            </div>
+        </form>
+
+        <h3>Delete Account</h3>
+        <form action="" method="post">
+            <div class="form-group">
+                <button type="submit" name="delete">Delete Account</button>
+            </div>
+        </form>
+    </div>
+</body>
+</html>

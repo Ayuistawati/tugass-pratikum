32e68a29d554767d0157bd0925c5a250d370e684
Made Nandika Saka Putra <115137019+NandikaSaka@users.noreply.github.com>
2023-06-20 08:55:36 +0800

    Update admin.php
    Versi fix

--- admin.php
+++ admin.php
@@ -1,4 +1,3 @@
-<!-- admin_dashboard.php -->
 <?php
 session_start();
 require_once 'config.php';
@@ -8,8 +7,27 @@ if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
     exit;
 }
 
-// ...
+// Mengambil data pengguna dari MongoDB
+$query = new MongoDB\Driver\Query([]);
+$result = $mongo->executeQuery('db_logreg.tb_register', $query);
 
+// Mengatur pilihan combo box
+$options = [
+    'all' => 'All Users',
+    'admin' => 'Admins',
+    'user' => 'Users'
+];
+
+// Mendapatkan data berdasarkan pilihan combo box
+if (isset($_GET['role'])) {
+    $selectedRole = $_GET['role'];
+    if ($selectedRole !== 'all') {
+        $query = new MongoDB\Driver\Query(['role' => $selectedRole]);
+        $result = $mongo->executeQuery('db_logreg.tb_register', $query);
+    }
+} else {
+    $selectedRole = 'all';
+}
 ?>
 
 <!DOCTYPE html>
@@ -21,35 +39,53 @@ if (!isset($_SESSION['username']) || $_SESSION['role'] !== 'admin') {
 <body>
     <div class="container">
         <h2>Welcome, <?php echo $_SESSION['username']; ?> (Admin)</h2>
-    <a href=""></a>
+        
+        <!-- Combo Box -->
+        <form action="" method="get">
+            <label for="role">Select Role:</label>
+            <select name="role" id="role">
+                <?php
+                foreach ($options as $value => $label) {
+                    $selected = ($selectedRole === $value) ? 'selected' : '';
+                    echo "<option value=\"$value\" $selected>$label</option>";
+                }
+                ?>
+            </select>
+            <button type="submit">Filter</button>
+        </form>
+        
         <!-- Data List -->
         <h3>Data List</h3>
-        <?php
-        // Query untuk mendapatkan data dari MongoDB
-        $query = new MongoDB\Driver\Query([]);
-        $result = $mongo->executeQuery('db_logreg.tb_register', $query);
-
-        echo '<table>';
-        echo '<tr><th>Username</th><th>Password</th><th>Gmail</th><th>Alamat</th><th>No. Telepon</th><th>Role</th><th>Action</th></tr>';
-        foreach ($result as $row) {
-            echo '<tr>';
-            echo '<td>' . $row->username . '</td>';
-            echo '<td>' . $row->password . '</td>';
-            echo '<td>' . $row->gmail . '</td>';
-            echo '<td>' . $row->alamat . '</td>';
-            echo '<td>' . $row->no_tlp . '</td>';
-            echo '<td>' . $row->role . '</td>';
-            echo '<td>';
-            echo '<a href="update.php?id=' . $row->_id . '">Update</a>';
-            echo ' | ';
-            echo '<a href="delete.php?id=' . $row->_id . '">Delete</a>';
-            echo '</td>';
-            echo '</tr>';
-        }
-        echo '</table>';
-        ?>
-
-        <a href="logout.php">Logout</a>
+        <table>
+            <tr>
+                <th>Username</th>
+                <th>Password</th>
+                <th>Gmail</th>
+                <th>Alamat</th>
+                <th>No. Telepon</th>
+                <th>Role</th>
+                <th>Action</th>
+            </tr>
+            <?php
+            foreach ($result as $row) {
+                echo '<tr>';
+                echo '<td>' . $row->username . '</td>';
+                echo '<td>' . $row->password . '</td>';
+                echo '<td>' . $row->gmail . '</td>';
+                echo '<td>' . $row->alamat . '</td>';
+                echo '<td>' . $row->no_tlp . '</td>';
+                echo '<td>' . $row->role . '</td>';
+                echo '<td>';
+                echo '<a href="update.php?id=' . $row->_id . '">Update</a>';
+                echo ' | ';
+                echo '<a href="delete.php?id=' . $row->_id . '">Delete</a>';
+                echo '</td>';
+                echo '</tr>';
+            }
+            ?>
+        </table>
+        
+        <a href="maindashboardadmin.php">Return</a>
     </div>
 </body>
 </html>
